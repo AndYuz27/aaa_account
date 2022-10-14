2082ad3a17f8c9c662978ebe7d98cc611608b7b8
AndYuz27 <yuzlov2003@gmail.com>
2022-10-14 16:57:47 +0300

    fff

---
+++ src/components/Profile/Stats.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="stat">
+<i v-if="type" :classs="'bi bi-heart'"></i>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Stats",
+    props: ["cnt", "type"]
+}
+</script>
+
+<style scoped>
+
+</style>
